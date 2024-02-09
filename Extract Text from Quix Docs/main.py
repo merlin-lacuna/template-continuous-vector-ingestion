f02d7cc2240e4f7165959692f76768e69534861b
Merlin Carter <merlin+hackathon@merlincarter.com>
2024-02-09 12:29:36 +0000

    Edited 'main.py'

--- Extract Text from Quix Docs/main.py
+++ Extract Text from Quix Docs/main.py
@@ -1,135 +1,63 @@
-### IMPORT THE STUFF
-from quix_docs_parser import quix_docs_extractor
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import re
-import logging
-import uuid
-import json
-
 import time
-from bs4 import BeautifulSoup, SoupStrainer
-from langchain_community.document_loaders import RecursiveUrlLoader, SitemapLoader, DirectoryLoader, BSHTMLLoader
-import os
+import uuid
+from random import randint, random, choice
+from time import sleep
+
+from dotenv import load_dotenv
 
 from quixstreams.kafka import Producer
-from quixstreams.platforms.quix import QuixKafkaConfigsBuilder, TopicCreationConfigs
 from quixstreams.models.serializers import (
     QuixTimeseriesSerializer,
     SerializationContext,
 )
+from quixstreams.platforms.quix import QuixKafkaConfigsBuilder, TopicCreationConfigs
 
+load_dotenv("./bank_example/quix_platform_version/quix_vars.env")
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-textchunksize = 4000
-textoverlapsize = 200
-
-### USING WEB CRAWLER
-# Inspired by: https://github.com/langchain-ai/chat-langchain/blob/master/ingest.py
-
-def metadata_extractor(meta: dict, soup: BeautifulSoup) -> dict:
-    title = soup.find("title")
-    description = soup.find("meta", attrs={"name": "description"})
-    html = soup.find("html")
-    return {
-        "source": meta["loc"],
-        "title": title.get_text() if title else "",
-        "description": description.get("content", "") if description else "",
-        "language": html.get("lang", "") if html else "",
-        **meta,
-    }
-
-def load_quix_docs():
-    return SitemapLoader(
-        "https://quix.io/docs/sitemap.xml",
-        filter_urls=["https://quix.io/docs/"],
-        parsing_function=quix_docs_extractor,
-        default_parser="lxml",
-        bs_kwargs={
-            "parse_only": SoupStrainer(
-                name="div", attrs={"class": "md-content"}
-            ),
-        },
-        meta_function=metadata_extractor,
-    ).load()
-
-def load_quix_docs_local():
-    html_loader_kwargs = {'open_encoding': 'utf-8'}
-    return DirectoryLoader(
-        r"C:\My Web Sites\QuixDocs\quix.io\docs",
-        glob="**/*.html",
-        loader_cls=BSHTMLLoader,
-        loader_kwargs=html_loader_kwargs
-    ).load()
-
-def simple_extractor(html: str) -> str:
-    soup = BeautifulSoup(html, "lxml")
-    return re.sub(r"\n\n+", "\n\n", soup.text).strip()
-
-def ingest_docs():
-    docs_from_documentation = load_quix_docs()
-    #docs_from_documentation = load_quix_docs_local()
-    logger.info(f"Loaded {len(docs_from_documentation)} docs from documentation")
-    logger.info("Logging first 5 docs..")
-    for d in range(5):
-        logger.info(f"Doc {d} is:\n {docs_from_documentation[d]} |")
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=textchunksize,
-        chunk_overlap=textoverlapsize)
-    docs_transformed = text_splitter.split_documents(
-        docs_from_documentation
-    )
-    logger.info(f"Docs after split {len(docs_transformed)}")
-    for d in range(5):
-        logger.info(f"SplitDoc {d} is:\n {docs_transformed[d]} |")
-
-    return docs_transformed
-
-
-quixdocs = ingest_docs()
-
-#### START QUIX STUFF ######
-outputtopicname = os.environ["output"]
-print(f"Producing to output topic: {outputtopicname}...\n\n")
 
 # For non-"Application.Quix" platform producing, config is a bit manual right now
-topic = outputtopicname
+topic = "qts__purchase_events"
 cfg_builder = QuixKafkaConfigsBuilder()
 cfgs, topics, _ = cfg_builder.get_confluent_client_configs([topic])
 topic = topics[0]
 cfg_builder.create_topics([TopicCreationConfigs(name=topic)])
 serialize = QuixTimeseriesSerializer()
 
-idcounter = 0
-with Producer(broker_address=cfgs.pop("bootstrap.servers"), extra_config=cfgs) as producer:
-    for doc in quixdocs:
-        doctext = re.sub(r'\n+', '\n', doc.page_content)
-        doctext = re.sub(r' +', ' ', doctext)
 
-        doc_id = idcounter
-        doc_key = f"A{'0'*(10-len(str(doc_id)))}{doc_id}"
-        doc_uuid = str(uuid.uuid4())
-        headers = {**serialize.extra_headers, "uuid": doc_uuid}
+retailers = [
+    "Billy Bob's Shop",
+    "Tasty Pete's Burgers",
+    "Mal-Wart",
+    "Bikey Bikes",
+    "Board Game Grove",
+    "Food Emporium",
+]
 
+
+# strings for key and headers will be serialized to bytes by default
+i = 0
+with Producer(
+    broker_address=cfgs.pop("bootstrap.servers"), extra_config=cfgs
+) as producer:
+    while i < 10000:
+        account = randint(0, 10)
+        account_id = f"A{'0'*(10-len(str(account)))}{account}"
+        headers = {**serialize.extra_headers, "uuid": str(uuid.uuid4())}
         value = {
+            "account_id": account_id,
+            "account_class": "Gold" if account >= 8 else "Silver",
+            "transaction_amount": randint(-2500, -1),
+            "transaction_source": choice(retailers),
             "Timestamp": time.time_ns(),
-            "doc_id": doc_id,
-            "doc_uuid": doc_uuid,
-            "doc_title": doc.metadata['title'],
-            "doc_content": doctext,
-            "doc_source": doc.metadata['source'],
         }
-        print(f"Producing value: {value}")
-        idcounter = idcounter + 1
+        print(f"Producing value {value}")
         producer.produce(
-            topic=outputtopicname,
-            headers=headers,  # a dict is also allowed here
-            key=doc_key,
+            topic=topic,
+            headers=headers,
+            key=account_id,
             value=serialize(
                 value=value, ctx=SerializationContext(topic=topic, headers=headers)
-            ),  # needs to be a string
+            ),
         )
-
-print("ingested quix docs")
+        i += 1
+        sleep(random())
